22bf0f4f9b68803ba4b838383818bda57025dcf8
ChiragSethi-1153 <chirag.1153@zenmonk.tech>
2024-02-01 11:44:15 +0530

    comiit

--- Backend/Controller/Controller.js
+++ Backend/Controller/Controller.js
@@ -15,7 +15,7 @@ exports.addData =  async(req, res) => {
 
 exports.getData =  async(req, res) => { 
     try{
-        const response = await findData(req.body);
+        const response = await findData(req);
         return res.status(200).json(response)
     }
     catch (error) {
--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -3,27 +3,39 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const Home = () => {
-    const [data , setData] = useState("")
+
+    const [data , setData] = useState([])
 
     useEffect(() => {
         const fetchData = async () => {
             const result = await axios.get('http://localhost:8080/api/users');
             setData(result.data);
+            console.log(result)
           };
           fetchData();
   }, []);
+
    return (
     <div>
-      <table >
-        <tr style={{gap: '0'}} >
-            <th style={{border: '1px solid black'}}> Sr. No.</th>
+      <table style={{border: '1px solid black'}}>
+        <tr >
+            
             <th style={{border: '1px solid black'}}>Name</th>
+            {
+                    data?.map((item) => {
+                        return <td style={{border: '1px solid black'}}>{item.name}</td>
+                    })
+                }
         </tr>
-
-        {data && <div>{JSON.stringify(data)}</div>}
         
-
+        <tr>
+            
+        <th style={{border: '1px solid black'}}> Sr. No.</th>
+            
+        </tr>
+        
       </table>
+      
     </div>
   )
 }
